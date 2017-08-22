f3ce7502fa54f38bb5b684139b27bc961b0a6ec4
vEnCa-X <venca-x@seznam.cz>
2017-08-22 20:15:21 +0200

    netteCodingStandard

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,22 @@ module.exports = function (grunt) {
     grunt.initConfig({
         shell: {
             test: {
-                //command: 'vendor\\bin\\tester.bat -c tests/php.ini tests'
                 command: 'vendor\\bin\\tester tests -s -p php -c tests\\php.ini'
+            },
+            installNetteCodingStandard: {
+                command: 'composer create-project nette/coding-standard nette-coding-standard'
+            },
+            netteCodingStandard: {
+                command: 'php nette-coding-standard\\\\ecs check src tests --config nette-coding-standard\\\\coding-standard-php71.neon'
+            },
+            netteCodingStandardFIX: {
+                command: 'php nette-coding-standard\\\\ecs check src tests --config nette-coding-standard\\\\coding-standard-php71.neon --fix'
             }
         }
     });
 
     grunt.registerTask('test', ['shell:test']);
+    grunt.registerTask('netteCodingStandard', ['shell:netteCodingStandard']);
+    grunt.registerTask('netteCodingStandardFIX', ['shell:netteCodingStandardFIX']);
 
 };
