46c9cbdb610413a8e4ad586fe24b77ae0ea37f98
vEnCa-X <venca-x@seznam.cz>
2017-08-25 16:03:40 +0200

    coding standard

--- src/NetteDateTimePicker.php
+++ src/NetteDateTimePicker.php
@@ -7,87 +7,87 @@ namespace VencaX\NetteDateTime;
 use Nette;
 use Nette\Forms\Controls;
 use Nette\Utils\DateTime;
-use Symfony\Component\Config\Definition\Exception\Exception;
 
 class NetteDateTimePicker extends Controls\TextInput
 {
-    /** @var Type date and time formats */
-    protected $type;
+	/** @var Type date and time formats */
+	protected $type;
 
-    /** @var DateTime */
-    protected $valueDateTime = null;
+	/** @var DateTime */
+	protected $valueDateTime;
 
-    private $supportFormatsArray = [
-        'datetime' => 'd.m.Y H:i',
-        'date' => 'd.m.Y',
-        'month' => 'm Y',
-        'time' => 'H:i',
-        'timesec' => 'H:i:s',
-    ];
-
-    public static function register()
-    {
-        \Nette\Forms\Container::extensionMethod('addDate', function (\Nette\Forms\Container $form, $name, $label = null, $type = 'datetime') {
-            $component = new NetteDateTimePicker($label, $type);
-            $form->addComponent($component, $name);
-            return $component;
-        });
-    }
+	private $supportFormatsArray = [
+		'datetime' => 'd.m.Y H:i',
+		'date' => 'd.m.Y',
+		'month' => 'm Y',
+		'time' => 'H:i',
+		'timesec' => 'H:i:s',
+	];
 
 
-    public function __construct(string $label = null, string $type)
-    {
-        if (!isset($this->supportFormatsArray[$type])) {
-            throw new \InvalidArgumentException("addDate: invalid date type '$type' given.");
-        }
-
-        parent::__construct($label);
-        $this->type = $type;
-        $this->control->addClass('nette-date-time');
-        $this->control->data('dateinput-type', $type);
-    }
+	public static function register()
+	{
+		\Nette\Forms\Container::extensionMethod('addDate', function (\Nette\Forms\Container $form, $name, $label = null, $type = 'datetime') {
+			$component = new NetteDateTimePicker($label, $type);
+			$form->addComponent($component, $name);
+			return $component;
+		});
+	}
 
 
-    public function getControl(): Nette\Utils\Html
-    {
-        $control = parent::getControl();
-        $control->value = $this->value;
-        return $control;
-    }
+	public function __construct(string $label = null, string $type)
+	{
+		if (!isset($this->supportFormatsArray[$type])) {
+			throw new \InvalidArgumentException("addDate: invalid date type '$type' given.");
+		}
+
+		parent::__construct($label);
+		$this->type = $type;
+		$this->control->addClass('nette-date-time');
+		$this->control->data('dateinput-type', $type);
+	}
 
 
-    public function setValue($value)
-    {
-        if ($value != null) {
+	public function getControl(): Nette\Utils\Html
+	{
+		$control = parent::getControl();
+		$control->value = $this->value;
+		return $control;
+	}
 
-            if ($value instanceof DateTime) {
-                $this->valueDateTime = $value;
-            } else {
-                //parse
-                $this->valueDateTime = DateTime::createFromFormat($this->supportFormatsArray[$this->type], $value);
 
-                if ($this->valueDateTime == null) {
-                    //default parser if is problem in first parser
-                    $this->valueDateTime = new DateTime($value);
-                }
-            }
+	public function setValue($value)
+	{
+		if ($value != null) {
+			if ($value instanceof DateTime) {
+				$this->valueDateTime = $value;
+			} else {
+				//parse
+				$this->valueDateTime = DateTime::createFromFormat($this->supportFormatsArray[$this->type], $value);
 
-            //value for input
-            $this->value = $this->valueDateTime->format($this->supportFormatsArray[$this->type]);
+				if ($this->valueDateTime == null) {
+					//default parser if is problem in first parser
+					$this->valueDateTime = new DateTime($value);
+				}
+			}
 
-        } else {
-            $this->value = null;
-        }
+			//value for input
+			$this->value = $this->valueDateTime->format($this->supportFormatsArray[$this->type]);
 
-        return $this;
-    }
+		} else {
+			$this->value = null;
+		}
 
-    /**
-     * Value after submit form
-     * @return DateTime
-     */
-    public function getValue()
-    {
-        return $this->valueDateTime;
-    }
+		return $this;
+	}
+
+
+	/**
+	 * Value after submit form
+	 * @return DateTime
+	 */
+	public function getValue()
+	{
+		return $this->valueDateTime;
+	}
 }
--- tests/GetHttpDataTest.phpt
+++ tests/GetHttpDataTest.phpt
@@ -8,64 +8,65 @@ use Nette\Forms\Form;
 use Tester;
 use Tester\Assert;
 use VencaX;
-use Nette;
 
 require __DIR__ . '/bootstrap.php';
 
 class GetHttpDataTest extends Tester\TestCase
 {
+	public function setUp()
+	{
+		VencaX\NetteDateTime\NetteDateTimePicker::register();
+		$_SERVER['REQUEST_METHOD'] = 'POST';
+		$_GET = $_POST = $_FILES = [];
+	}
 
-    public function setUp()
-    {
-        VencaX\NetteDateTime\NetteDateTimePicker::register();
-        $_SERVER['REQUEST_METHOD'] = 'POST';
-        $_GET = $_POST = $_FILES = [];
-    }
 
-    public function testSomething()
-    {
-        Assert::true(true);
-    }
+	public function testSomething()
+	{
+		Assert::true(true);
+	}
 
-    public function testMonth()
-    {
-        $form = new Form;
 
-        $form->addDate('month', 'Month:', 'month')->setValue("10.1.2017");
-        $form->addDate('month2', 'Month:', 'month')->setValue("1 2017");
+	public function testMonth()
+	{
+		$form = new Form;
 
-        $form->addSubmit('send', 'Send');
+		$form->addDate('month', 'Month:', 'month')->setValue('10.1.2017');
+		$form->addDate('month2', 'Month:', 'month')->setValue('1 2017');
 
-        Assert::truthy($form->isSubmitted());
-        Assert::true($form->isSuccess());
-        Assert::same([], $form->getHttpData());
+		$form->addSubmit('send', 'Send');
 
-        $a = $form->getValues(true);
-        Assert::equal("01 2017", $a["month"]->format("m Y"));
-        Assert::equal("01 2017", $a["month2"]->format("m Y"));
-    }
+		Assert::truthy($form->isSubmitted());
+		Assert::true($form->isSuccess());
+		Assert::same([], $form->getHttpData());
 
-    public function testDateAndTime()
-    {
-        $form = new Form;
+		$a = $form->getValues(true);
+		Assert::equal('01 2017', $a['month']->format('m Y'));
+		Assert::equal('01 2017', $a['month2']->format('m Y'));
+	}
 
-        $form->addDate('datetime', 'Date time:', 'datetime')->setValue("24.8.2017 15:42");
-        $form->addDate('date', 'Date:', 'date')->setValue("25.8.2017");
-        $form->addDate('time', 'Time:', 'time')->setValue("10:25");
-        $form->addDate('timesec', 'Time sec:', 'timesec')->setValue("12:42:41");
 
-        $form->addSubmit('send', 'Send');
+	public function testDateAndTime()
+	{
+		$form = new Form;
 
-        Assert::truthy($form->isSubmitted());
-        Assert::true($form->isSuccess());
+		$form->addDate('datetime', 'Date time:', 'datetime')->setValue('24.8.2017 15:42');
+		$form->addDate('date', 'Date:', 'date')->setValue('25.8.2017');
+		$form->addDate('time', 'Time:', 'time')->setValue('10:25');
+		$form->addDate('timesec', 'Time sec:', 'timesec')->setValue('12:42:41');
 
-        $a = $form->getValues(true);
+		$form->addSubmit('send', 'Send');
 
-        Assert::same("24.08.2017 15:42", $a["datetime"]->format('d.m.Y H:i'));
-        Assert::same("25.08.2017", $a["date"]->format('d.m.Y'));
-        Assert::same("10:25", $a["time"]->format('H:i'));
-        Assert::same("12:42:41", $a["timesec"]->format('H:i:s'));
-    }
+		Assert::truthy($form->isSubmitted());
+		Assert::true($form->isSuccess());
+
+		$a = $form->getValues(true);
+
+		Assert::same('24.08.2017 15:42', $a['datetime']->format('d.m.Y H:i'));
+		Assert::same('25.08.2017', $a['date']->format('d.m.Y'));
+		Assert::same('10:25', $a['time']->format('H:i'));
+		Assert::same('12:42:41', $a['timesec']->format('H:i:s'));
+	}
 }
 
 $test = new GetHttpDataTest();
