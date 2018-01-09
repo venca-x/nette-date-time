0f4caca119c70be1db500dba0cf4d51a3e5e0eb1
vEnCa-X <venca-x@seznam.cz>
2018-01-09 19:25:35 +0100

    improve Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,28 +5,29 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         shell: {
+            init: {
+                command: 'npm install',
+                command: 'composer update'
+            },
             test: {
                 command: 'vendor\\bin\\tester tests -s -p php'
             },
-            installCodeSoft: {
-                command: 'composer create-project nette/coding-standard nette-coding-standard',
-                command: 'composer create-project nette/code-checker nette-code-checker'
-            },
             netteCodeChecker: {
-                command: 'php nette-code-checker\\src\\code-checker.php -d src -d tests --short-arrays --strict-types'
+                command: 'php ..\\..\\nette-code-checker\\src\\code-checker.php -d src --short-arrays --strict-types',
             },
             netteCodeCheckerFIX: {
-                command: 'php nette-code-checker\\src\\code-checker.php -d src -d tests --short-arrays --strict-types --fix'
+                command: 'php ..\\..\\nette-code-checker\\src\\code-checker.php -d src --short-arrays --strict-types --fix',
             },
             netteCodingStandard: {
-                command: 'php nette-coding-standard\\ecs check src tests --config nette-coding-standard\\coding-standard-php71.neon'
+                command: 'php ..\\..\\nette-coding-standard\\bin\\easy-coding-standard check src tests --level clean-code'
             },
             netteCodingStandardFIX: {
-                command: 'php nette-coding-standard\\ecs check src tests --config nette-coding-standard\\coding-standard-php71.neon --fix'
+                command: 'php ..\\..\\nette-coding-standard\\bin\\easy-coding-standard check src tests --level clean-code --fix'
             }
         }
     });
 
+    grunt.registerTask('installDependencies', ['shell:init']);
     grunt.registerTask('test', ['shell:test']);
     grunt.registerTask('netteCodeChecker', ['shell:netteCodeChecker']);
     grunt.registerTask('netteCodeCheckerFIX', ['shell:netteCodeCheckerFIX']);
